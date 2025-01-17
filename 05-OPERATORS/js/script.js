c7531cf09acdd4050f82700ae34cf62264579bdc
Learn-GIT-cloud <dixitahir0050@gmail.com>
2025-01-17 15:55:09 +0530

    changes

---
+++ 05-OPERATORS/js/script.js
@@ -0,0 +1,18 @@
+// operators
+let num=96;
+let num_2=92;
+console.log(num+num_2);
+// unary opearator
+num++
+console.log(num+num_2);
+
+// assignment operator
+// it is binary
+let assign=99;
+
+// comparision operator
+num_comp=num==num_2
+console.log(num_comp);
+
+num_comp_2=num != num_2
+console.log(num_comp_2);
