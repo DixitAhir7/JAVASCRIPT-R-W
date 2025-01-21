ee9e5c027b134a2b5f41d99c61dcfff5c44a05ca
Learn-GIT-cloud <dixitahir0050@gmail.com>
2025-01-21 15:01:12 +0530

    chanes

---
+++ 07-CASTING/script.js
@@ -0,0 +1,16 @@
+let str="15.37";
+let str_num=parseInt(str);
+console.log(str_num);
+
+let str_number=Number(str);
+console.log(str_number);
+
+let str_float=parseFloat(str);
+console.log(str_float);
+
+let str_bool=Boolean(str);
+console.log(str_bool);
+
+let str_zero="0";
+let str_con_zero=Boolean(str_zero);
+console.log(str_con_zero);
