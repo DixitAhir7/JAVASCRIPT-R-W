dd71d16a7b6b28ed51cf67cb227584292f054413
Learn-GIT-cloud <dixitahir0050@gmail.com>
2025-01-24 19:39:54 +0530

    changes

--- 08-IF-ELSE-STATEMENT/js/ie.js
+++ 08-IF-ELSE-STATEMENT/js/ie.js
@@ -11,3 +11,12 @@ else{
 let Name="dixit";
 Name++
 console.log(Name);
+
+let a = 20;
+let b = 30;
+
+if (a<b || a>b) {
+    console.log("a is greater tthan b");
+}else{
+    console.log("a is smaller than b");
+}
--- PRACTICE/js/pr.js
+++ PRACTICE/js/pr.js
@@ -1,31 +1,56 @@
 let x=3;
-console.log(x+3);
-console.log(x-3);
 console.log(x*3);
 console.log(x/3);
 console.log(x %3);
-console.log(x++);
-console.log(x);
 console.log(x**3);
-console.log(++x);
+
+//in post increment or decrease you'll have to print it 
+//in pre you won't have to print it
+
 console.log(x--);
 console.log(x);
 console.log(--x);
-
-console.log(2==2);
 console.log(2==3);
 console.log("2"==2);
 console.log(2=="4");
 console.log(2==="2");
 console.log(2!=3);
-console.log(3!=3);
-console.log(3!=3);
-console.log(2!=4);
 console.log(2===2);
-console.log(2==="2");
 console.log(3>=3);
 console.log(3<=3);
-console.log(2>=3);
 console.log(6>=3);
 console.log(34<=44);
-console.log(33<=3);
+
+//swapping two variables
+
+let a = 10;
+let b = 20;
+
+[a,b] = [b,a];
+console.log(a);
+console.log(b);
+
+num=Number(prompt("enter a num"))
+num_2=Number(prompt("enter a second num"))
+
+if (num,num_2==0){
+    console.log("please enter a number");
+}else if (num>num_2){
+    console.log("minimum number:",num_2);
+}else if (num<num_2){
+    console.log("minimum number:",num);
+}else{
+    console.log("goodbye");
+}
+
+//neutral number
+
+zero=Number(prompt("enter a number:"))
+
+if (zero==0){
+    console.log("it is zero");
+}else if(zero>0){
+    console.log("it's postive");
+}else if (zero<0){
+    console.log("it's negative");
+}
