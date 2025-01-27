8e856156110c60d93da8960e895cb4bc53632c31
Learn-GIT-cloud <dixitahir0050@gmail.com>
2025-01-27 21:49:09 +0530

    changes

--- 09-switch/js/sw.js
+++ 09-switch/js/sw.js
@@ -1,71 +1,104 @@
-let user=Number(prompt("enter a number:"));
+// let user=Number(prompt("enter a number:"));
 
-switch (user){
-    case 1:console.log("sun");
-    break;
+// switch (user){
+//     case 1:console.log("sun");
+//     break;
 
-    case 2:console.log("mon");
-    break;
+//     case 2:console.log("mon");
+//     break;
 
-    case 3:console.log("tue");
-    break;
+//     case 3:console.log("tue");
+//     break;
 
-    case 4:console.log("wed");
-    break;
+//     case 4:console.log("wed");
+//     break;
 
-    case 5:console.log("thu");
-    break;
+//     case 5:console.log("thu");
+//     break;
 
-    case 6:console.log("fri");
-    break;
+//     case 6:console.log("fri");
+//     break;
 
-    case 7:console.log("sat");
-    break;
+//     case 7:console.log("sat");
+//     break;
     
-    default:{
-        console.log("invalid");
-    }
-}
+//     default:{
+//         console.log("invalid");
+//     }
+// }
 
-let num=prompt("enter a positive num:");
-num > 0 ? console.log("you r right") : num < 0 ? console.log("it's negative") : console.log("it is zero");
+// let num=prompt("enter a positive num:");
+// num > 0 ? console.log("you r right") : num < 0 ? console.log("it's negative") : console.log("it is zero");
 
 
 //telecome service
 
-console.log("press 1 for english");
-console.log("press 2 for hindi");
-console.log("press 3 for gujrati");
+
+function telecoe_service(){
+    let lang, service;
+    console.log("1 english");
+    console.log("2 hindi");
+    console.log("3 gujrati");
+    lang = Number(prompt("enter ur language:"));
 
 
-let user_service=Number(prompt("enter your language"));
 
+switch(lang){
+    case 1 :
+        console.log("top-up");
+        console.log("special");
+        console.log("net pack");
+        service = Number(prompt("enter your service:"));
+        switch(service){
+            case 1 :console.log("you have done recharge");
+            break;
 
-switch(user_service){
-    case 1 :console.log("you choose english");
+            case 2:console.log("you have done recharge");
+            break;
 
-    case 2: console.log("you choose hindi");
+            case 3 :console.log("you have done recharge");
+        }   
+    ;
+    case 2 :
+        console.log("top-up");
+        console.log("special");
+        console.log("net pack");
+        service = Number(prompt("enter your service:"));
+        switch(service){
+            case 1 :console.log("safaltapurav recharge");
+            break;
 
-    case 3 : console.log("you chooose gujrati");
+            case 2:console.log("safaltapurav recharge");
+            break;
+
+            case 3 :console.log("safaltapurav recharge");
+        }   
+    ;
+    case 3 :
+        console.log("top-up");
+        console.log("special");
+        console.log("net pack");
+        service = Number(prompt("enter your service:"));
+        switch(service){
+            case 1 :console.log("thay gayu");
+            break;
+
+            case 2:console.log("thay gayu");
+            break;
+
+            case 3 :console.log("thay gayu");
+        }   
+    ;
+    
 }
 
 
-let enter_choice=prompt("enter your choice");
 
-switch(enter_choice){
-    case 1:console.log("you choose internet recharge");
-
-    case 2 : console.log("you choose call recharge");
-
-    case 3 : console.log("you choose specail recharge");
-
-    default:{
-        console.log("you have succesfully done a recharge");
-    }
 }
+    telecoe_service();
 
-// calculator
+// // calculator
 
-let A=Number(prompt("enter any number:"));
-let B=Number(prompt("enter second value:"));
-let c =A+B
+// let A=Number(prompt("enter any number:"));
+// let B=Number(prompt("enter second value:"));
+// let c =A+B
