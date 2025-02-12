d6bb1e2da58305008c60945c4bf2bdad221e1e8f
Learn-GIT-cloud <dixitahir0050@gmail.com>
2025-02-12 22:04:42 +0530

    changes

--- 11-FOR_LOOP/js/script.js
+++ 11-FOR_LOOP/js/script.js
@@ -16,34 +16,3 @@
 // };
 
 
-/*
-when you make fun in object and print it out the value if you did clg and it will be undefined,
-but if you return it it will return a value
-*/  
-
-
-let obj={
-    name:"dixit",
-    age:20,
-    city:"Mangrol",
-    country:"India",
-    /*
-    this keyword will work only in defined object{}
-    if you have 5 object with different obj name but ,
-    key names are same then you can use (this) keyword
-    */ 
-   
-    fun:function(){
-        console.log(this.age);
-        return "hello"
-    }
-};
-
-result=obj.fun()
-console.log(result);
-
-i=0;
-while(i<5){
-    console.log("hello",i);
-    i++;
-}
