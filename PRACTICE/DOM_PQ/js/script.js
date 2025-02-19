7955d2d8d0ae9d103e1a88807cded55443776004
for_work <p69947042@gmail.com>
2025-02-19 18:30:49 +0530

    changes

--- PRACTICE/DOM_PQ/js/script.js
+++ PRACTICE/DOM_PQ/js/script.js
@@ -1,22 +1,18 @@
 let count = 0;
 
 function increment() {
-    document.getElementsByClassName("counter1").innerText = count;
-    count++;
-    console.log(count)
+    let count_add = document.getElementsByClassName("counter1")[0];
+    count_add.innerText = ++count;
 }
-
-let count_decrement = 0;
 
 function decrement() {
-    document.getElementsByClassName("counter1").innerText = count_decrement;
-    count_decrement--;
-    console.log(count_decrement)
+    let count_add = document.getElementsByClassName("counter1")[0];
+    count_add.innerText = --count;
+
 }
 
-let count_reset = 0;
-
 function reset() {
-    document.getElementsByClassName("counter1").innerText = count_reset
-    console.log(count_reset)
+    count = 0;
+    let count_add = document.getElementsByClassName("counter1")[0];
+    count_add.innerText = count;
 }
