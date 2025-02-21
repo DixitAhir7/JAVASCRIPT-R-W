d4c4ae55ad136db397cee77682eda34a7a3c7d6b
for_work <p69947042@gmail.com>
2025-02-21 14:04:27 +0530

    changes

--- PRACTICE/DOM-QUESTIONS/DOM-FORM/js/script.js
+++
@@ -1,9 +0,0 @@
-// let changeColor = document.getElementsByClassName("container");
-// document.body.style.backgroundColor = changeColor.value;
-
-const h2Tage = document.querySelector("h2");
-const inputColor = document.querySelector(".container");
-
-function changeColor() {
-    h2Tage.style.backgroundColor = inputColor.value
-}
