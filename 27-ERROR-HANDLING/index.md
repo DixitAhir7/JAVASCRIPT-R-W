42e71546dddc733b4642bce113c0ddd1ba6dfe03
dixit-ahir <dixitahir0050@gmail.com>
2025-04-02 10:53:28 +0530

    error handling

---
+++ 27-ERROR-HANDLING/index.md
@@ -0,0 +1,3 @@
+<!-- why use error handling  -->
+
+-->because it make sure that not all functions after the try,catch block will stop
--- 27-ERROR-HANDLING/js/script.js
+++ 27-ERROR-HANDLING/js/script.js
@@ -2,16 +2,30 @@ import * as names from "./main.js";
 
 names.name();
 
+// try {
+//     function add() {
+//         let addition = 8 + 5;
+//         console.log("Addition:", addition);
+//         add();
+//     }
+
+//     add();
+// } catch (e) {
+
+//     // messagee will run the speicific error
+//     console.log("Error caught:", e.message);
+// }
+
+
 try {
-    function add() {
-        let addition = 8 + 5;
-        console.log("Addition:", addition);
-        add();
+    let user = prompt('enter name: ');
+    console.log(user);
+
+
+    // will throw error
+    if (!user) {
+        throw new Error("can't be empty");
     }
-
-    add();
 } catch (e) {
-
-    // messagee will run the speicific error
-    console.log("Error caught:", e.message);
+    console.log(e);
 }
