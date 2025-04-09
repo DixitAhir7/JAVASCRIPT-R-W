488c181d73595cfb4f2947847a7d3b0ab3038560
dixit-ahir <dixitahir0050@gmail.com>
2025-04-09 10:31:03 +0530

    inheritance in oops

---
+++ OOPS/inheritance/js/index.md
@@ -0,0 +1,4 @@
+--> inheritance in js allows you to create 1 parent class and let you use parent class properties in child class
+
+--> to acess class properties user (extents);
+--> to use properties use super() that is built in function for parent child relation classs
---
+++ OOPS/inheritance/js/script.js
@@ -0,0 +1,38 @@
+class User {
+
+    #id;
+    static countId = 0;
+
+    constructor(name, age, contact) {
+        this.name = name
+        this.age = age
+        this.contact = contact
+        User.countId++
+        this.#id = User.countId
+    };
+
+    getId() {
+        return this.#id
+    }
+};
+
+class Student extends User {
+    constructor(id, name, age, contact, subjects = []) {
+        super(name, age, contact);
+        this.id = id
+        this.subjects = subjects
+    };
+};
+
+class Teacher extends User {
+    constructor(id, name, age, contact, subjects = []) {
+        super(name, age, contact);
+        this.id = id
+        this.subjects = subjects
+    };
+};
+
+
+const s1 = new Student('dixit', 21, 8866555078, 'javascript');
+s1.getId();
+console.log(s1);
